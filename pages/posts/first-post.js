9213cc4b81c22fe305cdb9a0e5d0a8c6d469e04c
Gustavo Gutierrez <gusguti04@gmail.com>
2021-05-27 12:15:04 -0700

    Add image and play with layout components

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import Layout from '../../components/layout';
 
 const firstPost = () => {
   return (
     <>
-      <h1>First Post</h1>
-      <h2>
+      <Layout>
+
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <Image
+          src='/images/profile.jpg'
+          height={144}
+          width={144}
+          alt="Rand pic"
+        />
+        <h1 className="tag">First Post</h1>
         <Link href="/">
           <a>Back to home</a>
         </Link>
-      </h2>
+
+        {/* <style jsx>{`
+         .tag {
+            background: red;
+         }
+      `}</style> */}
+      </Layout>
     </>
   );
 };
